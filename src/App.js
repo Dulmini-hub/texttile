189592cda0341fc4e3ed27732cefdde4603af0e6
Dulmini <imdulmini@gmail.com>
2024-09-11 22:54:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Apps from './components/Apps';
+
+
+export default function App() {
+  return (
+    <div className="app">
+     <Apps/> 
+    </div>
+  );
+}
+
+
